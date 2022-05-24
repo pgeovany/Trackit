7267a75f1381c633b4ce738c61cab62da863838f
pgeovany <pgeovany@dcc.ufrj.br>
2022-05-24 20:52:24 -0300

    log in screen done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,26 +1,50 @@
-import logo from "../../assets/images/Logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
+import axios from "axios";
 import styled from "styled-components";
+import logo from "../../assets/images/Logo.svg";
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [disableButton, setDisableButton] = useState(false);
+    const [userData, setUserData] = useState([]);
+    const navigate = useNavigate();
+
+    function logUser(e) {
+        e.preventDefault();
+        setDisableButton(true);
+        const body = {
+            email,
+            password
+        }
+        const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", body);
+        request.then(answer => {
+            setUserData(answer.data);
+            navigate("/hoje");
+        })
+        request.catch(({response}) => {
+            setDisableButton(false);
+            alert(response.data.message);
+        })
+    }
+
     return(
         <Container>
             <img src={logo} alt="logo"/>
-            <Form />
+            <form onSubmit={logUser}>
+                <Input required disabled={disableButton} value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="email"/>
+                <Input required disabled={disableButton} value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="senha"/>
+                <Button type="submit" disabled={disableButton}>
+                    {disableButton ? <ThreeDots color="white"/> : "Entrar"}
+                </Button>
+            </form>
             <Link to="/cadastro">
                 <p>Não tem uma conta? Cadastre-se!</p>
             </Link>
         </Container>
-    );
-}
-
-function Form () {
-    return (
-            <form>
-                <Input required type="email" placeholder="email"/>
-                <Input required type="password" placeholder="senha"/>
-                <Button>Entrar</Button>
-            </form>
     );
 }
 
@@ -52,7 +76,8 @@ const Container = styled.div`
 
 const Input = styled.input`
     font-family: "Lexend", sans-serif;
-    background-color: #FFFFFF;
+    background-color: ${props => props.disabled ? "#F2F2F2" : "#FFFFFF"};
+    color: ${props => props.disabled ? "#AFAFAF" : "black"};
     border: 1px solid #D5D5D5;
     border-radius: 5px;
     height: 45px;
@@ -74,6 +99,9 @@ const Button = styled.button`
     text-decoration: none;
     font-size: 20px;
     border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     &:hover {
         cursor: pointer;
     }
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
 import axios from "axios";
 import styled from "styled-components";
 import logo from "../../assets/images/Logo.svg";
-import { ThreeDots } from "react-loader-spinner";
 
 export default function SignUp() {
 
