fd68e215ac99fc5ebe47d241c6cc47412bf447d7
pgeovany <pgeovany@dcc.ufrj.br>
2022-05-24 16:36:13 -0300

    basic routing structure done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import Login from "../Components/Login/Login";
+import SignUp from "../Components/SignUp/SignUp";
+import Habits from "../Components/Habits/Habits";
+import CurrentDay from "../Components/CurrentDay/CurrentDay";
+import HabitsHistory from "../Components/HabitsHistory/HabitsHistory";
+
 import "../assets/css/reset.css";
 import "../assets/css/style.css";
 
 export default function App() {
     return (
-        <></>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Login />}/>
+                <Route path="/cadastro" element={<SignUp />}/>
+                <Route path="/habitos" element={<Habits />}/>
+                <Route path="/hoje" element={<CurrentDay />}/>
+                <Route path="/historico" element={<HabitsHistory />}/>
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     );
 }
---
+++ src/Components/HabitsHistory/HabitsHistory.js
@@ -0,0 +1,7 @@
+export default function HabitsHistory() {
+    return(
+        <>
+            HabitsHistory component
+        </>
+    );
+}
